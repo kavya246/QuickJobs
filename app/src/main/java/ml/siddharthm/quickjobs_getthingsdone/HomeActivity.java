126272e4ff4b89f29d2e662bffc8728bf0266caf
Siddharth M <siddharth.m98@gmail.com>
2019-01-29 11:13:37 +0530

    Nav Drawer Layout Allignment

--- app/src/main/java/ml/siddharthm/quickjobs_getthingsdone/HomeActivity.java
+++ app/src/main/java/ml/siddharthm/quickjobs_getthingsdone/HomeActivity.java
@@ -91,12 +91,6 @@ public class HomeActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
