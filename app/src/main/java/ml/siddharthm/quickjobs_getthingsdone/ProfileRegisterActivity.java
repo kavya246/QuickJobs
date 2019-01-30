2f777379c51586008b921cf280cf39e7915eb044
Siddharth M <siddharth.m98@gmail.com>
2019-01-30 22:57:31 +0530

    Creating the Details Registration Activity

--- app/src/main/java/ml/siddharthm/quickjobs_getthingsdone/ProfileRegisterActivity.java
+++ app/src/main/java/ml/siddharthm/quickjobs_getthingsdone/ProfileRegisterActivity.java
@@ -6,19 +6,19 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
 
-public class MyProfileDetailsActivity extends AppCompatActivity {
+public class ProfileRegisterActivity extends AppCompatActivity {
 
     private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_profile_details);
+        setContentView(R.layout.activity_profile_register);
 
         myToolbar = findViewById(R.id.my_profile_toolbar);
         if (myToolbar!=null) {
             setSupportActionBar(myToolbar);
-            getSupportActionBar().setTitle("My Profile");
+            getSupportActionBar().setTitle("Register");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
             myToolbar.setNavigationIcon(R.drawable.back_btn);
@@ -30,6 +30,5 @@ public class MyProfileDetailsActivity extends AppCompatActivity {
             });
 
         }
-
     }
 }
