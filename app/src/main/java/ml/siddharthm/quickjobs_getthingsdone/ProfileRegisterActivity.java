c886522a4a04fe3fd8753a8ecebc2bbc05468480
Siddharth M <siddharth.m98@gmail.com>
2019-01-31 22:31:15 +0530

    Cropping Image and adding profile pictureto firebase storage

--- app/src/main/java/ml/siddharthm/quickjobs_getthingsdone/ProfileRegisterActivity.java
+++ app/src/main/java/ml/siddharthm/quickjobs_getthingsdone/ProfileRegisterActivity.java
@@ -1,12 +1,14 @@
 package ml.siddharthm.quickjobs_getthingsdone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -19,8 +21,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 
@@ -34,6 +44,9 @@ public class ProfileRegisterActivity extends AppCompatActivity {
     private Button createAccountButton;
    private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
+    private static int galleryPic = 1;
+    private StorageReference UserProfileImageRef;
+    private String currentUserId;
 
 
     @Override
@@ -43,6 +56,8 @@ public class ProfileRegisterActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         RootRef = FirebaseDatabase.getInstance().getReference();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
         createAccountButton = (Button)findViewById(R.id.profile_register_button);
         myToolbar = findViewById(R.id.my_profile_toolbar);
@@ -67,6 +82,16 @@ public class ProfileRegisterActivity extends AppCompatActivity {
         profileAddress = (EditText)findViewById(R.id.profile_register_address);
         profilePhone = (TextView)findViewById(R.id.profile_register_phone);
 
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,galleryPic);
+            }
+        });
+
         String newString;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
@@ -89,7 +114,10 @@ public class ProfileRegisterActivity extends AppCompatActivity {
         });
 
 
+
     }
+
+
 
 
     private void CreateNewAccount() {
@@ -132,5 +160,53 @@ public class ProfileRegisterActivity extends AppCompatActivity {
 
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==galleryPic && resultCode==RESULT_OK && data!=null){
+            Uri imageUri = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            final CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK) {
+
+
+                final Uri resultUri = result.getUri();
+                StorageReference filepath = UserProfileImageRef.child(currentUserId + ".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ProfileRegisterActivity.this, "Image uploaded succesfully...", Toast.LENGTH_SHORT).show();
+                            final String downloadUrl = task.getResult().getDownloadUrl().toString();
+                            RootRef.child("Users").child(currentUserId).child("image").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(ProfileRegisterActivity.this, "Image saved in db succesfully", Toast.LENGTH_SHORT).show();
+                                        ((CircleImageView) findViewById(R.id.profile_register_picture)).setImageURI(resultUri);
+
+                                    } else {
+                                        Toast.makeText(ProfileRegisterActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(ProfileRegisterActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                });
+            }}
+    }
+
 
 }
